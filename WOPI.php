40b65a1381a99096e1adc3ef596c6f234e45e219
Chris <the.bitpoet@gmail.com>
2018-04-11 19:29:13 +0200

    Add locking support and field settings
    Add support for WOPI locks, even if not used by Collabora CODE(?).
    Add configuration option to file field settings to enable editing (also
    in template context).
    Minor code cleanups and adding comments.
    Bump versions to 0.0.8.

--- WOPI.php
+++ WOPI.php
@@ -1,5 +1,12 @@
 <?php namespace ProcessWire;
 
+/**
+ * Handles WOPI requests and file locks for the Collabora CODE
+ * WOPI client.
+ *
+ * Code by BitPoet
+ * Licensed under MPL 2.0
+ */
 class LoolWopi extends WireData {
 	
 	const lockTableName = "lool_locks";
@@ -12,8 +19,6 @@ class LoolWopi extends WireData {
 			return array("error" => "HTTP/1.1 400 Illegal URL path");
 		}
 		
-		$this->log->message("Segments: " . print_r($segments, true));
-
 		if($segments[1] == "files")	{
 			// files endpoint
 			$ret = array("endpoint" => "file", "action" => "NotImplemented");
@@ -43,8 +48,15 @@ class LoolWopi extends WireData {
 					case "PUT_RELATIVE":
 						$ret["action"] = "PutRelativeFile";
 						break;
+					*/
 					case "LOCK":
-						$ret["action"] = "Lock";
+						if(isset($_SERVER["HTTP-X-WOPI-OLDLOCK"])) {
+							$ret["action"] = "UnlockAndRelock";
+							$ret["lock"] = $_SERVER["HTTP-X-WOPI-OLDLOCK"];
+						} else {
+							$ret["action"] = "Lock";
+							$ret["lock"] = isset($_SERVER["HTTP-X-WOPI-LOCK"]) ? $_SERVER["HTTP-X-WOPI-LOCK"] : "";
+						}
 						break;
 					case "UNLOCK":
 						$ret["action"] = "Unlock";
@@ -52,7 +64,6 @@ class LoolWopi extends WireData {
 					case "REFRESH_LOCK":
 						$ret["action"] = "RefreshLock";
 						break;
-					*/
 					default:
 						break;
 				}
@@ -98,8 +109,63 @@ class LoolWopi extends WireData {
 		return array("error" => "Unknown endpoint");
 	}
 	
+	
+	public function getLock($fileid) {
+		$db = $this->database;
+		
+		$sql =	"SELECT *, (CASE WHEN " .
+				"lock_update_time = 0 THEN " .
+				"TIMESTAMPDIFF(SECOND, lock_create_time, NOW()) ELSE " .
+				"TIMESTAMPDIFF(SECOND, lock_update_time, NOW()) " .
+				"END) as age " .
+				" FROM " . self::lockTableName .
+				" WHERE fileid = " . $db->quote($fileid);
+		
+		$ret = $db->query($sql);
+		
+		if(! $ret) return false;
+		
+		if(! $row = $ret->fetch(\PDO::FETCH_ASSOC)) return false;
+		
+		// Expire locks after 30 minutes = 1800 seconds
+		if($row["age"] > 1800) {
+			$this->unLock($fileid);
+			return false;
+		}
+		
+		return $row;
+	}
+	
+	
+	public function lock($fileid, $user) {
+		$db = $this->database;
+		
+		$sql =	"INSERT INTO " . self::lockTableName . " ( " .
+				" fileid, lock_user_id " .
+				" ) VALUES ( " .
+				$db->quote($fileid) . ", " . $user->id .
+				")";
+		
+		$db->exec($sql);
+	}
+	
+	public function unLock($fileid) {
+		$db = $this->database;
+		
+		$sql =	"DELETE FROM " . self::lockTableName . " WHERE fileid = " . $db->quote($fileid);
+		$db->exec($sql);
+	}
+	
+	public function updateLock($fileid) {
+		$db = $this->database;
+
+		$sql =	"UPDATE " . self::lockTableName . " SET lock_update_time = NOW()";
+		$db->exec($sql);
+	}
+	
 	public function createLocksTable() {
 		$sql =	"CREATE TABLE IF NOT EXISTS " . self::lockTableName . " ( " .
+				" id int unsigned not null auto_increment, " .
 				" fileid varchar(100), " .
 				" lock_user_id int, " .
 				" lock_create_time timestamp default current_timestamp, " .
@@ -117,4 +183,8 @@ class LoolWopi extends WireData {
 		$this->database->exec($sql);
 	}
 	
+	public function addLockIdColumn() {
+		$sql = "ALTER TABLE " . self::lockTableName . " ADD COLUMN id int unsigned not null auto_increment FIRST, ADD KEY(id)";
+		$this->database->exec($sql);
+	}
 }
